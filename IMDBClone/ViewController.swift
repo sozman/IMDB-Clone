b77285001bcff3755eb2626867c0d75a88e42d88
Sinan Özman <sinanozman@icloud.com>
2021-02-11 20:34:15 +0300

    Project Init

---
+++ IMDBClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDBClone
+//
+//  Created by Sinan Özman on 11.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
